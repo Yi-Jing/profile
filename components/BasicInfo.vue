2c5819a6037756b00a4593d922474bc4a4873239
Yi-Jing <meg778899@gmail.com>
2024-08-02 17:08:14 +0800

    fix: 個人簡介文案跟頭貼大小

--- components/BasicInfo.vue
+++ components/BasicInfo.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="flex flex-col items-center justify-center text-white">
     <div class="relative text-sm font-bold after:content-[''] after:block after:w-[80px] after:h-[2px] after:absolute after:-left-[7px] after:bg-[#e4c9fe] after:mt-[2px]">個 人 簡 介</div>
-    <div class="w-26 h-26 my-4">
+    <div class="w-32 h-32 my-4">
       <img class="shadow-sm rounded-md" :src="info.avatar"/>
     </div>
-    <h2 class="font-bold">{{ info.name }}</h2>
-    <div class="text-xs mt-2">{{ info.age }}</div>
-    <div class="text-xs">{{ info.school }}</div>
+    <h2 class="text-lg font-bold">{{ info.name }}</h2>
+    <div class="text-xs mt-2">{{ info.skill }}</div>
   </div>
 </template>
 
@@ -14,8 +13,7 @@
 
 const info = {
   name: '陳 怡 靜',
-  age: '28歲',
   avatar: '/images/陳怡靜_1 比 1.jpg',
-  school: '私立長榮大學 資訊管理學系'
+  skill: '擅長應用 Vue.js 專注於網頁開發'
 }
 </script>
