c8fc3871b0daf4cf68c3740b012de8df9abc2f13
Yi-Jing <meg778899@gmail.com>
2024-08-02 10:40:25 +0800

    feat: 個人簡介元件檔

---
+++ components/BasicInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col items-center justify-center text-white">
+    <div class="relative text-sm font-bold after:content-[''] after:block after:w-[80px] after:h-[2px] after:absolute after:-left-[7px] after:bg-[#e4c9fe] after:mt-[2px]">個 人 簡 介</div>
+    <div class="w-26 h-26 my-4">
+      <img class="shadow-sm rounded-md" :src="info.avatar"/>
+    </div>
+    <h2 class="font-bold">{{ info.name }}</h2>
+    <div class="text-xs mt-2">{{ info.age }}</div>
+    <div class="text-xs">{{ info.school }}</div>
+  </div>
+</template>
+
+<script>
+
+const info = {
+  name: '陳 怡 靜',
+  age: '28歲',
+  avatar: '/images/陳怡靜_1 比 1.jpg',
+  school: '私立長榮大學 資訊管理學系'
+}
+</script>
---
+++ pages/about.md
@@ -0,0 +1,8 @@
+
+<div class="flex">
+<!-- e4c9fe -->
+  <div class="left-area absolute top-0 left-0 bg-[#c6ace0] w-1/3 h-full flex items-center justify-center">
+    <BasicInfo/>
+  </div>
+
+</div>
