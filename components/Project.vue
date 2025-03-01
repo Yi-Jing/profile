d0532e2c72903ee4207c292a621c340f3271bf90
Yi-Jing <meg778899@gmail.com>
2025-03-01 17:21:48 +0800

    feat: 新增專案智慧代理的資訊

--- components/Project.vue
+++ components/Project.vue
@@ -3,24 +3,29 @@
     <div v-click="1" class="text-sm text-stone-500 text-left mb-2 w-full">
       <span class="mr-4">專案介紹</span>
     </div>
-  
+
     <div>
-      <div
-        v-for="(project, index) in projects"
-        :key="project.name"
-        v-click
-        >
+      <div v-for="(project, index) in projects" :key="project.name" v-click>
         <template v-if="isShow(index)">
           <div class="flex justify-start gap-5 mb-3">
             <div class="w-[40%] h-fit my-4">
-              <img class="shadow-md rounded-md object-contain" :src="project.image"/>
+              <img
+                class="shadow-md rounded-md object-contain"
+                :src="project.image"
+              />
             </div>
             <div class="flex flex-col items-start">
               <div class="flex items-center gap-2 mt-6">
                 <span class="text-xl font-bold">{{ project.name }}</span>
-                <Icon class="mb-[2px]" :path="linkIcon" @click="linkTo(project.link)" />
+                <Icon
+                  class="mb-[2px]"
+                  :path="linkIcon"
+                  @click="linkTo(project.link)"
+                />
               </div>
-              <span class="mt-2 text-sm text-stone-500">{{ project.content }}</span>
+              <span class="mt-2 text-sm text-stone-500">{{
+                project.content
+              }}</span>
             </div>
           </div>
         </template>
@@ -30,46 +35,46 @@
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
-import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router'
+import { ref, watch } from 'vue'
 
 const route = useRoute()
 const projects = [
   {
+    name: '智慧代理',
+    image: new URL('/images/project-cover/TCoeus.png', import.meta.url),
+    content: '提供 AI 聊天機器人協助企業解決營運相關問題',
+    link: '',
+  },
+  {
     name: '行銷自動化系統',
     image: new URL('/images/project-cover/Linx.png', import.meta.url),
     content: '促使百貨公司的行銷人員能自動對顧客發送廣告的系統',
-    link: 'https://yi-jing.github.io/linx-file/1'
+    link: 'https://yi-jing.github.io/linx-file/1',
   },
   {
     name: '高雄智慧走跑',
     image: new URL('/images/project-cover/kaush.png', import.meta.url),
     content: '宣傳高雄市政府舉辦走跑活動的一頁式網站',
-    link: 'https://kcg-running.tw/'
+    link: 'https://kcg-running.tw/',
   },
   {
     name: '新北災訓 E 點通',
     image: new URL('/images/project-cover/disterinfo.png', import.meta.url),
     content: '可即時查看新北市周遭附近災情的 SPA 網站',
-    link: 'https://e.ntpc.gov.tw/news-info'
+    link: 'https://e.ntpc.gov.tw/news-info',
   },
   {
     name: '食在營養',
     image: new URL('/images/project-cover/nutrition.png', import.meta.url),
     content: '傳達營養師團隊服務的 SSR 網站',
-    link: 'https://nutrition.tw/'
+    link: 'https://nutrition.tw/',
   },
   {
     name: 'TSNA 體育新聞團隊',
     image: new URL('/images/project-cover/tsna.png', import.meta.url),
     content: '整理體育新聞的 SSR 網站',
-    link: 'https://tsna.com/'
-  },
-  {
-    name: '花食間 手作',
-    image: new URL('/images/project-cover/Hua Shin Jian.png', import.meta.url),
-    content: '提供美好餐飲的咖啡廳的 SPA 網站',
-    link: 'https://objective-haibt-83b3b9.netlify.app/'
+    link: 'https://tsna.com/',
   },
 ]
 
@@ -78,7 +83,7 @@ watch(
   () => route.query?.clicks,
   (val) => {
     currentClicks.value = val
-  },
+  }
 )
 
 const isShow = (index) => {
